51fc9f3be684342ccd7bf9ecc06c3a03997328f9
Nish Lalgee <lalgee.nish@gmail.com>
2017-09-27 15:26:34 -0700

    Add stylesheet and edit views. Create db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,42 @@
 
 Product.delete_all # This was left out the first time in order to keep what I typed in by hand using the web form.
 
-Product.create!(title: 'Seven mobile apps in seven weeks',
+Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
   description:
     %{<p>
-      <em>Native Apps, Multiple Platforms.</em>
-      Answer the question "Can we build this for ALL the devices?" with 
-      a ressounding YES.
+      <em>Native Apps, Multiple Platforms</em>
+      Answer the question “Can we build this for ALL the devices?” with a
+      resounding YES. This book will help you get there with a real-world
+      introduction to seven platforms, whether you’re new to mobile or an
+      experienced developer needing to expand your options. Plus, you’ll find
+      out which cross-platform solution makes the most sense for your needs.
       </p>},
   image_url: '7apps.jpg',
   price: 26.00)
+
+
+Product.create!(title: 'Another book',
+  description:
+    %{<p>
+      <em>Native Apps, Multiple Platforms.</em>
+      Answer the question "Can we build this for ALL the devices?" with
+      a ressounding YES.
+      </p>},
+  image_url: 'adrpo.jpg',
+  price: 32.00)
+
+  Product.create!(title: 'Rails, Angular, Postgres, and Bootstrap',
+    description:
+      %{<p>
+        <em>Powerful, Effective, and Efficient Full-Stack Web Development</em>
+        As a Rails developer, you care about user experience and performance,
+        but you also want simple and maintainable code. Achieve all that by
+        embracing the full stack of web development, from styling with
+        Bootstrap, building an interactive user interface with AngularJS, to
+        storing data quickly and reliably in PostgreSQL. Take a holistic view of
+        full-stack development to create usable, high-performing applications,
+        and learn to use these technologies effectively in a Ruby on Rails
+        environment.
+        </p>},
+    image_url: 'dcbang.jpg',
+    price: 45.00)
