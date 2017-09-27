58f8c3d468d1db98588f20a4c182c7a98a6817b2
Nish Lalgee <lalgee.nish@gmail.com>
2017-09-26 19:40:48 -0700

    Create seed products using seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all # This was left out the first time in order to keep what I typed in by hand using the web form.
+
+Product.create!(title: 'Seven mobile apps in seven weeks',
+  description:
+    %{<p>
+      <em>Native Apps, Multiple Platforms.</em>
+      Answer the question "Can we build this for ALL the devices?" with 
+      a ressounding YES.
+      </p>},
+  image_url: '7apps.jpg',
+  price: 26.00)
