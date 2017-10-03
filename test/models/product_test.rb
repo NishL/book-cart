2ccd970cf8338589e475699e5f3afb86b9f0d411
Nish Lalgee <lalgee.nish@gmail.com>
2017-10-03 17:03:45 -0400

    Test length validator and test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,13 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "title is at least 10 characters long" do
+    prod = Product.new(title: '123',
+                       description: "yyy",
+                       price: 1,
+                       image_url: 'fred.gif')
+    assert prod.invalid?
+    assert prod.errors[:title].any?
+    assert_equal ["That title isn't long enough"], prod.errors[:title]
+  end
 end
