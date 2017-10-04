8c7041a9facc960a6f301299e8d58fceded518f3
Nish Lalgee <lalgee.nish@gmail.com>
2017-10-03 23:27:06 -0700

    Edit controller to list alphabetically and edit view

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,7 @@
 class StoreController < ApplicationController
   def index
+    # The customer wants titles displayed in alphabetical order.
+    # Achieve this by calling `order(:title)` on the  `Product` model.
+    @products = Product.order(:title)
   end
 end
