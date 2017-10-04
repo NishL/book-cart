6516d9bb7be915e0a65d9369cf40787791e4fd8f
Nish Lalgee <lalgee.nish@gmail.com>
2017-10-03 20:31:20 -0400

    Generate Store controller and view, modify root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index' # Using `as:` allows the existing tests to continue working. Rails creates `store_index_path` & `store_index_url` accessot methods.
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
