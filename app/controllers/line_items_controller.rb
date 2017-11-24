7cc8c04851ab929a7aaf44929f80d615a842c006
Nish Lalgee <lalgee.nish@gmail.com>
2017-11-23 23:30:30 -0500

    Make cart respond to AJAX request

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,6 +32,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url } # remove the flash message, notice: 'Line item was successfully created.' } # Add `.cart` to the method call, the line item object knows how to find the cart object
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
