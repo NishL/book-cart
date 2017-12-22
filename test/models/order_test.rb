e02100a39cea264fbe3c4cf92d6eaadf37d2fc3e
Nish Lalgee <lalgee.nish@gmail.com>
2017-12-22 01:45:58 -0500

    Add order validations and one test

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,4 +4,8 @@ class OrderTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "name must not be blank" do
+    order = Order.new(name: '')
+    assert order.invalid?
+  end
 end
