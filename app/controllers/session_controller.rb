f63dec4cb78e6eeb1da5bc8385dcb084b7487c7c
Nish Lalgee <lalgee.nish@gmail.com>
2018-01-11 15:34:08 -0500

    Use safe traversal operator instead of Rails try() method which does the same thing

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,7 @@ class SessionController < ApplicationController
     # We need to record something in the session to show that an administrator is logged in.
     # We will store the ID of that person's `User` object using the the key `:user_id` in the session hash.
     user = User.find_by(name: params[:name]) # The user enters their username in the form
-    if user.try(:authenticate, params[:password]) # If the username is found then try() to authenticate their password
+    if user&.authenticate(params[:password]) # If the username is found then try to authenticate their password.
       session[:user_id] = user.id # If the username was found and the password matches, then add :user_id to the session object.
       redirect_to admin_url # After authenication redirect to the admin_url page.
     else
@@ -23,5 +23,8 @@ end
 # How the create() action works:
 # 1) First, find the user by their name when it's passed in through the accepted params for a User object. Save it in a variable.
 # 2) Use the Rails try() method to check if a variable has a value of `nil` before trying to call the method.
+#    -change to `user&.authenticate(params[:password])`, this is in the Ruby core instead of Rails ActiveSupport,
+#     it's called the "safe travaersal" or "safe navigation" operator, it checks if an object is nil (user in this case) and returns `nil`
+#     instead of a "NoMethodError: undefined method `authenticate' for nil:NilClass"
 # 3) If the user is authenticated then store the user ID in the session object and redirect them to the admin page.
 # 4) If the user is not authenticated then redirect them to the login page again.
