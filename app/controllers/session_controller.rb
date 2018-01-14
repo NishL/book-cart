7b51eec25a437d982fe6dd44f800fca27afb4f15
Nish Lalgee <lalgee.nish@gmail.com>
2018-01-14 02:21:06 -0500

    Fix form rendering error and add admin sidebar

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,7 +7,7 @@ class SessionController < ApplicationController
     # We need to record something in the session to show that an administrator is logged in.
     # We will store the ID of that person's `User` object using the the key `:user_id` in the session hash.
     user = User.find_by(name: params[:name]) # The user enters their username in the form
-    if user&.authenticate(params[:password]) # If the username is found then try to authenticate their password.
+    if user.try(:authenticate, params[:password]) # If the username is found then try to authenticate their password.
       session[:user_id] = user.id # If the username was found and the password matches, then add :user_id to the session object.
       redirect_to admin_url # After authenication redirect to the admin_url page.
     else
