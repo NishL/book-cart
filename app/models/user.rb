2f3275de73f87caccd777e0070aab23c36a20c04
Nish Lalgee <lalgee.nish@gmail.com>
2018-01-14 23:57:07 -0500

    Add code to prevent deleting the lasst admin (implement ActiveRecord hook, raising error and rescuing error)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true # Check the name is present and unique (no two users with same name in the DB)
   has_secure_password
+
+  after_destroy :ensure_an_admin_remains # Use an ActiveRecord hook method to roll back a transaction
+
+  class Error < StandardError
+  end
+
+  private
+
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise Error, "Can't delete the last user!"
+    end
+  end
 end
