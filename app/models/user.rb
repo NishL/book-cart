689c50d076dd19d2c07e2993b7edb2a5ca7218c7
Nish Lalgee <lalgee.nish@gmail.com>
2018-01-16 00:21:24 -0500

    Add note to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
 
   after_destroy :ensure_an_admin_remains # Use an ActiveRecord hook method to roll back a transaction
 
+  # This error will get rescued in the users controller
   class Error < StandardError
   end
 
   private
 
+  # Raise an error when the admin count is zero. In combination with the after_destroy() ActiveRecord hook
+  # the transaction will get rolled back.
   def ensure_an_admin_remains
     if User.count.zero?
       raise Error, "Can't delete the last user!"
