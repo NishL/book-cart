7fbdfc9e08ba4b3b6ed9c2f795ea6bd3d16c0296
Nish Lalgee <lalgee.nish@gmail.com>
2018-01-17 00:07:38 -0500

    Start i18n, set deafault and set up routes

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,15 @@
+#encoding: utf-8
+I18n.defualt_locale = :en
+
+LANGUAGES = [
+  ['English', 'en'],
+  ['Espa&ntilde;ol'.html_safe, 'es']
+]
+
+# This code does two things:
+# 1) It uses the I18n module to set the defualt_locale.
+# 2) It defines a list of associations between display names and locale names.
+#    We don't have a keyboard that can input a tilde above the 'n' in Espanol,
+#    So we call html_safe on the string so that Rails knows the string is safe
+#    to be interpreted as containing HTML, otherwise only the markup would be
+#    displayed. 
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,12 @@ Rails.application.routes.draw do
   resources :products do
     get :who_bought, on: :member # Try it out: `curl --silent http://localhost:3000/products/3/who_bought.atom`
   end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
