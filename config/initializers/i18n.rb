38636b755b06f2d85e3674b9c060e5c372a51cde
Nish Lalgee <lalgee.nish@gmail.com>
2018-01-17 16:14:07 -0500

    Fix error in router and i18n locale config

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,5 @@
 #encoding: utf-8
-I18n.defualt_locale = :en
+I18n.default_locale = :en
 
 LANGUAGES = [
   ['English', 'en'],
@@ -12,4 +12,4 @@ LANGUAGES = [
 #    We don't have a keyboard that can input a tilde above the 'n' in Espanol,
 #    So we call html_safe on the string so that Rails knows the string is safe
 #    to be interpreted as containing HTML, otherwise only the markup would be
-#    displayed. 
+#    displayed.
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index' # Using `as:` allows the existing tests to continue working. Rails creates `store_index_path` & `store_index_url` accessor methods.
 
   resources :products do
     get :who_bought, on: :member # Try it out: `curl --silent http://localhost:3000/products/3/who_bought.atom`
@@ -22,6 +21,7 @@ Rails.application.routes.draw do
     resources :line_items
     resources :carts
     root 'store#index', as: 'store_index', via: :all
+    #root 'store#index', as: 'store_index' # Using `as:` allows the existing tests to continue working. Rails creates `store_index_path` & `store_index_url` accessor methods.
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
