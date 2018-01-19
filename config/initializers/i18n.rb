e5bbadff2b42621c83a93d2bf03e8078d7a237a5
Nish Lalgee <lalgee.nish@gmail.com>
2018-01-18 22:37:17 -0500

    Add French translations

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,7 +3,8 @@ I18n.default_locale = :en
 
 LANGUAGES = [
   ['English',                  'en'],
-  ["Espa&ntilde;ol".html_safe, 'es']
+  ["Espa&ntilde;ol".html_safe, 'es'],
+  ["French",                   'fr']
 ]
 
 # This code does two things:
