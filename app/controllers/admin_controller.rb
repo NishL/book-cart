4fd76ddf3528a1e167c9d62a84dfb57724e3f143
Nish Lalgee <lalgee.nish@gmail.com>
2018-01-18 22:25:33 -0500

    Correct errors in translation configuration files, all translations now working

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,7 @@
 class AdminController < ApplicationController
   def index
     @total_orders = Order.count
-    user = User.find_by(params[:name])
+    user = User.find_by(params[:name]) # This may be vulnerable to SQL injection
     @user_name = user.name
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   def set_i18n_locale_from_params
     if params[:locale]
       if I18n.available_locales.map(&:to_s).include?(params[:locale])
-        I18n.local = params[:locale]
+        I18n.locale = params[:locale]
       else
         flash.now[:notice] = "#{params[:locale]} translation is not available"
         logger.error flash.now[:notice]
